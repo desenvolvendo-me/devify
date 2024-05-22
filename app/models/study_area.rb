7784342d2dc00e23ab1c338bbbfe9484bb19cf7f
eskokado <eskokado@gmail.com>
2024-05-22 17:42:46 -0300

    feat(study area): model

---
+++ app/models/study_area.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: study_areas
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class StudyArea < ApplicationRecord
+end
