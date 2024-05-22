6bf7ef0e75b32689f39e5b1041ac338c02c9ae3a
eskokado <eskokado@gmail.com>
2024-05-22 17:43:54 -0300

    feat: schema of programming language and study area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_22_114711) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_22_194953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,12 +106,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_22_114711) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "programming_languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "student_progresses", force: :cascade do |t|
     t.date "date"
     t.string "description"
     t.string "programming_language"
     t.string "study_area"
     t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "study_areas", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
