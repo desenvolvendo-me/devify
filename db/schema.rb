1118cfe680bd0b98b8eae8e3c951feeeae75d0c2
eskokado <eskokado@gmail.com>
2024-05-22 16:38:56 -0300

    build: resolve conflit into schema

--- db/schema.rb
+++ db/schema.rb
@@ -156,6 +156,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_29_131222) do
     t.index ["user_id"], name: "index_student_profiles_on_user_id"
   end
 
+  create_table "student_progresses", force: :cascade do |t|
+    t.date "date"
+    t.string "description"
+    t.string "programming_language"
+    t.string "study_area"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
