8266bbfc1d609dfe6c3e57179989157afff59d5e
eskokado <eskokado@gmail.com>
2024-05-22 17:48:04 -0300

    feat(home controller): add selected filter by programming language or study area

--- app/controllers/manager/home_controller.rb
+++ app/controllers/manager/home_controller.rb
@@ -1,9 +1,24 @@
 module Manager
   class HomeController < InternalController
     def index
-      @student_progress_data = StudentProgress
-                                 .group_by_week(:date, format: "%d/%b/%Y")
-                                 .sum(:value)
+      progress_data = StudentProgress.group_by_week(:date, format: "%d/%b/%Y")
+
+      if params[:language].present?
+        progress_data = progress_data.where(programming_language: params[:language])
+      end
+
+      if params[:area].present?
+        progress_data = progress_data.where(study_area: params[:area])
+      end
+
+      progress_data = progress_data.sum(:value)
+
+      limited_data = progress_data.first(30).to_h
+
+      @student_progress_data = limited_data
+
+      @languages = ProgrammingLanguage.all
+      @areas = StudyArea.all
     end
   end
 end
