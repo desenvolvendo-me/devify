7b32c89d8e769883a86f52898e140a7ea561c9cb
eskokado <eskokado@gmail.com>
2024-05-22 17:41:54 -0300

    feat(programming language): model

---
+++ app/models/programming_language.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: programming_languages
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ProgrammingLanguage < ApplicationRecord
+end
