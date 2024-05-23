b53f4abe1fc5aa9b5a5972ca02a57e1c2e4b754e
eskokado <eskokado@gmail.com>
2024-05-23 15:38:47 -0300

    build: resolve conflit

--- db/schema.rb
+++ db/schema.rb
@@ -133,11 +133,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_22_135011) do
   create_table "student_progresses", force: :cascade do |t|
     t.date "date"
     t.string "description"
-    t.string "programming_language"
-    t.string "study_area"
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "programming_language_id"
+    t.bigint "study_area_id"
+    t.index ["programming_language_id"], name: "index_student_progresses_on_programming_language_id"
+    t.index ["study_area_id"], name: "index_student_progresses_on_study_area_id"
   end
 
   create_table "study_areas", force: :cascade do |t|
@@ -185,5 +187,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_22_135011) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "student_progresses", "programming_languages"
+  add_foreign_key "student_progresses", "study_areas"
   add_foreign_key "student_profiles", "users"
 end
