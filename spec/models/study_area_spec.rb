2c44e65427c61049f1ccb1903ec4c544a60aac94
eskokado <eskokado@gmail.com>
2024-05-23 16:06:07 -0300

    test(study areas) is valid with a valid name and is invalid without a name

--- spec/models/study_area_spec.rb
+++ spec/models/study_area_spec.rb
@@ -10,5 +10,14 @@
 require 'rails_helper'
 
 RSpec.describe StudyArea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a valid name" do
+    programming_language = build(:programming_language)
+    expect(programming_language).to be_valid
+  end
+
+  it "is invalid without a name" do
+    programming_language = build(:programming_language, name: nil)
+    programming_language.valid?
+    expect(programming_language.errors[:name]).to include("não pode ficar em branco")
+  end
 end
