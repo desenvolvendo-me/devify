4df5239137a4c63b14623a9239608502b51a86db
eskokado <eskokado@gmail.com>
2024-05-23 16:14:18 -0300

    feat(student progress) factory

--- spec/factories/student_progresses.rb
+++ spec/factories/student_progresses.rb
@@ -2,21 +2,31 @@
 #
 # Table name: student_progresses
 #
-#  id                   :bigint           not null, primary key
-#  date                 :date
-#  description          :string
-#  programming_language :string
-#  study_area           :string
-#  value                :integer
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id                      :bigint           not null, primary key
+#  date                    :date
+#  description             :string
+#  value                   :integer
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  programming_language_id :bigint
+#  study_area_id           :bigint
+#
+# Indexes
+#
+#  index_student_progresses_on_programming_language_id  (programming_language_id)
+#  index_student_progresses_on_study_area_id            (study_area_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (programming_language_id => programming_languages.id)
+#  fk_rails_...  (study_area_id => study_areas.id)
 #
 FactoryBot.define do
   factory :student_progress do
-    date { "2024-05-22" }
-    description { "MyString" }
-    programming_language { "MyString" }
-    study_area { "MyString" }
-    value { 1 }
+    date { FFaker::Time.date }
+    description { FFaker::Lorem.sentence }
+    value { rand(1..100) }
+    association :programming_language
+    association :study_area
   end
 end
