305dfd8bab1c32e515e8833b81d33130141072fb
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-05-28 14:11:25 -0300

    feat: start project simulation

--- app/controllers/project_simulations_controller.rb
+++ app/controllers/project_simulations_controller.rb
@@ -1,26 +1,31 @@
 class ProjectSimulationsController < ApplicationController
-  # GET /project_simulations
-  def index
+  before_action :authenticate_user!
+    def index
     @project_simulations = ProjectSimulation.all
   end
 
-  # GET /project_simulations/:id
   def show
     @project_simulation = ProjectSimulation.find(params[:id])
   end
 
-  # GET /project_simulations/:id/new
   def new
-    @project_simulation = ProjectSimulation.find(params[:id])
-    current_student.project_simulations << @project_simulation
-    redirect_to @project_simulation, notice: 'Projeto iniciado com sucesso!'
+    @project_simulation = ProjectSimulation.new
   end
 
-  private
+    def create
+      @project_simulation = ProjectSimulation.find(params[:project_id])
+      current_user.project_simulations << @project_simulation
 
-  def current_student
-    # Este método deve ser implementado para retornar o aluno atualmente logado
-    # Isso pode ser feito, por exemplo, usando Devise ou um método próprio de autenticação
-    Student.first # Exemplo simplificado, substitua com a lógica de autenticação real
-  end
+      respond_to do |format|
+        format.html { redirect_to project_simulations_path, notice: 'Projeto iniciado com sucesso.' }
+        format.json { render :show, status: :created, location: @project_simulation }
+      end
+    end
+
+    private
+
+    def project_simulation_params
+      params.require(:project_simulation).permit(:project_id)
+    end
+
 end
