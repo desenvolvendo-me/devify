cabbc551c4ab63e7463c5cd6eba9d5b2a1aaf377
eskokado <eskokado@gmail.com>
2024-05-29 11:50:38 -0300

    fix(prepare chart): add filter mark

--- app/businesses/home/prepare_chart.rb
+++ app/businesses/home/prepare_chart.rb
@@ -1,21 +1,25 @@
 module Home
   class PrepareChart < BusinessApplication
-    def initialize(programming_language, study_area)
+    def initialize(mark, programming_language, study_area)
+      @mark = mark
       @programming_language = programming_language
       @study_area = study_area
     end
 
     def call
-      prepare_data(@programming_language, @study_area)
+      prepare_data(@mark, @programming_language, @study_area)
     end
 
     private
 
-    def prepare_data(programming_language, study_area)
-      progress_data = StudentProgress.group_by_week(:date, format: "%d/%b/%Y")
+    def prepare_data(mark, programming_language, study_area)
+      progress_data = StudentProgress.group_by_week(:date, format: '%d/%b/%Y')
+
+      progress_data = progress_data.where('mark <= ?', mark) if mark.present?
 
       if programming_language.present?
-        progress_data = progress_data.where(programming_language_id: programming_language)
+        progress_data = progress_data
+                        .where(programming_language_id: programming_language)
       end
 
       if study_area.present?
@@ -24,7 +28,7 @@ module Home
 
       progress_data = progress_data.sum(:value)
 
-      limited_data = progress_data.first(30).to_h
+      progress_data.to_h
     end
   end
 end
