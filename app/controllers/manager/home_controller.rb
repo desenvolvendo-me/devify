90f27ba694764b3b68e5e3cd4f938be5310ecf4c
eskokado <eskokado@gmail.com>
2024-06-01 10:42:40 -0300

    feat: fix home#index

--- app/controllers/manager/home_controller.rb
+++ app/controllers/manager/home_controller.rb
@@ -1,19 +1,6 @@
 module Manager
   class HomeController < InternalController
-    before_action :set_student_progress_data, only: :index
-
     def index
-      @languages = ProgrammingLanguage.all
-      @areas = StudyArea.all
-      @marks = StudentProgress.select(:mark).distinct.order(:mark).pluck(:mark)
-    end
-
-    private
-
-    def set_student_progress_data
-      @student_progress_data = Home::PrepareChart
-                               .new(params[:mark], params[:language], params[:area])
-                               .call
     end
   end
 end
