143ab98e327d7e4de9f811eeb68d163658811f6f
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-06-04 11:46:51 -0300

    test: project simulation model

--- spec/models/project_simulation_spec.rb
+++ spec/models/project_simulation_spec.rb
@@ -1,18 +1,14 @@
-# == Schema Information
-#
-# Table name: project_simulations
-#
-#  id               :bigint           not null, primary key
-#  description      :text
-#  expected_results :text
-#  objectives       :text
-#  title            :string
-#  tools            :text
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#
+# spec/models/project_simulation_spec.rb
 require 'rails_helper'
 
 RSpec.describe ProjectSimulation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_and_belong_to_many(:users) }
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:project_simulation)).to be_valid
+    end
+  end
 end
