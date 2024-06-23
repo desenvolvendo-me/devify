fcf4875d8d7581957dc3a3c7fc190a21591e3303
Pjmaciel <pjmaciel_4@hotmail.com>
2024-06-23 20:21:10 -0300

    fix(controller): update ProfilesController with new metrics and profile validation

--- app/controllers/manager/students/profiles_controller.rb
+++ app/controllers/manager/students/profiles_controller.rb
@@ -13,6 +13,10 @@ module Manager
         @skills = metrics_service.skills
         @language_mastery = metrics_service.language_mastery
         @exposure = metrics_service.exposure
+        @survey_level = metrics_service.calculate_survey_level
+
+        profile_metrics_service = ::Students::ProfileMetricsService.new(@student_profile)
+        @profile_level = profile_metrics_service.calculate_profile_level
       end
 
       def edit
