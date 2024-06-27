9b1066ae58de87b6d06f393828c79f5cb2baf76b
eskokado <eskokado@gmail.com>
2024-06-27 09:56:09 -0300

    feat: add card of level evolution

--- app/controllers/manager/dashboard_controller.rb
+++ app/controllers/manager/dashboard_controller.rb
@@ -1,9 +1,10 @@
 module Manager
   class DashboardController < InternalController
+    before_action :set_user, only: :index
     before_action :set_student_progress_data, only: :index
+    before_action :set_level, only: :index
 
     def index
-      @user = current_user
       @languages = ProgrammingLanguage.all
       @areas = StudyArea.all
       @marks = StudentProgress.select(:mark).distinct.order(:mark).pluck(:mark)
@@ -15,6 +16,15 @@ module Manager
       @student_progress_data = Home::PrepareChart
                                .new(@user, params[:mark], params[:language], params[:area])
                                .call
+    end
+
+    def set_level
+      @level = ::Students::ProfileMetricsService
+                 .new(@user.student_profile).calculate_profile_level
+    end
+
+    def set_user
+      @user = current_user
     end
   end
 end
