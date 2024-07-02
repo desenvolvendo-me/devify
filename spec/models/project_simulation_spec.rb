9c074b59e4c93da7ab33cc992231c9207f33487b
eskokado <eskokado@gmail.com>
2024-07-02 12:57:55 -0300

    [ISSUE#25] Permitir que o aluno entenda as evoluções feat: impl views partial modal feat: add view partial modal_survey feat: add view partial modal_profile feat: add view partial modal_style test: add comments into project_simulation_spec.rb

--- spec/models/project_simulation_spec.rb
+++ spec/models/project_simulation_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: project_simulations
+#
+#  id               :bigint           not null, primary key
+#  complexity       :string
+#  description      :text
+#  difficulty       :string
+#  expected_results :text
+#  objectives       :text
+#  title            :string
+#  tools            :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe ProjectSimulation, type: :model do
