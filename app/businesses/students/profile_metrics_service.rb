a2b4fb6b550df21bede7c8c4c68c419f35f66f33
Eriton Búrigo <eritonsburigo@gmail.com>
2024-09-08 18:55:04 -0300

    Descarta as alterações e retorna ao codigo implementado originalmente na release

--- app/businesses/students/profile_metrics_service.rb
+++ app/businesses/students/profile_metrics_service.rb
@@ -9,26 +9,7 @@ module Students
       # por exemplo, com base em campos como `main_language`, `track`, etc.
       # retorne um número que represente o nível do perfil
       # substitua a lógica abaixo com a lógica real de cálculo
-        @student_profile.level || 1  # Retorna o nível atual ou 1 como padrão
+      6
     end
 
-    def unlock_content_based_on_level
-      level = calculate_profile_level
-
-      # Lógica de desbloqueio de conteúdo baseada no nível calculado
-      case level 
-      when 1
-          unlock_level1_content
-          # Inserir outros niveis
-      else
-        puts "No content unlocked!"
-      end
-    end
-
-    # Método para desbloquear o conteúdo do nível 1
-    def unlock_level1_content
-      puts "content for level 1 unlocked"
-    end
-
-  end
-end
+    
